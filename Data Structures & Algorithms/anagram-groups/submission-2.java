99d1c85cf75aaf116f634372a1f59232f4fdb358
Siddhant Swaroop <77961403+siddh8nt@users.noreply.github.com>
2026-08-10 03:05:25 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>> map = new HashMap<>();
+        
+
+        for(String str:strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String ele = new String(chars);
+
+            map.putIfAbsent(ele,new ArrayList<>());
+            map.get(ele).add(str);
+
+        }
+        
+
+        return new ArrayList<>(map.values());
+    }
+}
+//SUBOPTIMAL METHOD NOW TRY MAKING 26 CHARACTER INDEXED ARRAY AS THE KEY FOR HASHMAP AS SORTING CAUSES THIS SOLUTION TO BECOME O(N*KLOGK) WHERE K IS THE SIZE OF MAXIMUM STRING LENGTH 
